9642e936a9cb001a26753b9ca52dbed9368b2709
capta1nseal <aidan@lunesdale.community>
2024-06-13 08:15:08 +0300

    Implement empty application wrapper class, and basic structs and classes for ray-based math operations

---
+++ src/application.hpp
@@ -0,0 +1,19 @@
+#ifndef _RAYTRACERAPPLICATION_
+#define _RAYTRACERAPPLICATION_
+
+
+/*
+A wrapper class doing nothing but guiding the execution of the program.
+*/
+class RayTracerApplication
+{
+public:
+    RayTracerApplication();
+
+    void run();
+
+private:
+};
+
+
+#endif
